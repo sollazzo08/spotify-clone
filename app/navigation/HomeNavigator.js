58a8f0dd77d802d58ddf78c5edb0e6bba326fc69
sollazzo <msollazzo08@gmail.com>
2021-04-04 22:04:33 -0400

    customized header options

--- app/navigation/HomeNavigator.js
+++ app/navigation/HomeNavigator.js
@@ -3,13 +3,30 @@ import { View, StyleSheet } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SettingsScreen from '../screens/SettingsScreen';
 import HomeScreen from '../screens/HomeScreen';
+import colors from '../config/colors';
 
 function HomeNavigator() {
   const HomeStack = createStackNavigator();
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-      <HomeStack.Screen name="Settings" component={SettingsScreen}/>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <HomeStack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          headerStyle: {
+            backgroundColor: colors.grey,
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: colors.white
+          },
+        }}
+      />
     </HomeStack.Navigator>
   );
 }
